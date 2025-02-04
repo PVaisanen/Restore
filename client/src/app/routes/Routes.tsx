be6b9b3908559d9535db77e4a7047de79fad7842
PVaisanen <pasivaisanen@hotmail.com>
2025-02-04 14:40:16 +0200

    session 7 end

--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -1,10 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "../loyout/App";
 import HomePage from "../../features/home/HomePage";
 import Catalog from "../../features/catolog/Catalog";
 import ProductDetails from "../../features/catolog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
+import ServerError from "../../app/errors/ServerError";
+import NotFound from "../../app/errors/NotFound";
 
 export const router = createBrowserRouter([
     {
@@ -16,10 +18,17 @@ export const router = createBrowserRouter([
             {path: '/catalog/:id', element: <ProductDetails />},
             {path: '/about', element: <AboutPage />},
             {path: '/contact', element: <ContactPage />},
+            {path: '/server-error', element: <ServerError />},
+            {path: '/not-found', element: <NotFound />},
+            {path: '*', element: <Navigate replace to='/not-found' />},
         ]
     }
 ], {
     future: {
-        v7_relativeSplashPath: true
+        v7_relativeSplashPath: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7skipActionErrorRevalidation: true
     }
 })
